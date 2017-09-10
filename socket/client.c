b0fc8e3e2915b17778a60e5fa8f4850fa48c838d
lichengcheng <lichengcheng@pset.suntec.net>
2017-09-10 17:45:05 +0800

    add log input
    Change-Id: I96b7665005792fbffeb255ac38b420dd1667de99

--- socket/client.c
+++ socket/client.c
@@ -10,31 +10,29 @@
 #define MAXLINE 1024
 int main(int argc,char **argv)
 {
-char *servInetAddr = "127.0.0.1";
-int socketfd;
-struct sockaddr_in sockaddr;
-char recvline[MAXLINE], sendline[MAXLINE];
-int n;
-  
-socketfd = socket(AF_INET,SOCK_STREAM,0);
-memset(&sockaddr,0,sizeof(sockaddr));
-sockaddr.sin_family = AF_INET;
-sockaddr.sin_port = htons(10004);
-inet_pton(AF_INET,servInetAddr,&sockaddr.sin_addr);
-if((connect(socketfd,(struct sockaddr*)&sockaddr,sizeof(sockaddr))) < 0 )
-{
-printf("connect error %s errno: %d\n",strerror(errno),errno);
-}
-
-printf("send message to server\n");
-strcpy(sendline, "Hello server, I am client. \n");
-if((send(socketfd,sendline,strlen(sendline),0)) < 0)
-{
- printf("send mes error: %s errno : %d",strerror(errno),errno);
-
-}
- 
-close(socketfd);
-printf("client exit\n");
-return 0;
+	char *servInetAddr = "127.0.0.1";
+	int socketfd;
+	struct sockaddr_in sockaddr;
+	char recvline[MAXLINE], sendline[MAXLINE];
+	int n;
+	socketfd = socket(AF_INET,SOCK_STREAM,0);
+	memset(&sockaddr,0,sizeof(sockaddr));
+	sockaddr.sin_family = AF_INET;
+	sockaddr.sin_port = htons(10004);
+	inet_pton(AF_INET,servInetAddr,&sockaddr.sin_addr);
+	if((connect(socketfd,(struct sockaddr*)&sockaddr,sizeof(sockaddr))) < 0 ) {
+		printf("connect error %s errno: %d\n",strerror(errno),errno);
+	}
+	
+	printf("send message to server\n");
+	strcpy(sendline, "Hello server, I am client. \n");
+	if((send(socketfd,sendline,strlen(sendline),0)) < 0) {
+		printf("send mes error: %s errno : %d",strerror(errno),errno);
+	}
+	else {
+		printf("send mes success\n");
+	}
+	close(socketfd);
+	printf("client exit\n");
+	return 0;
 }
