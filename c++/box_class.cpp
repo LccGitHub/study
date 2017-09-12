22e6865a50a47f10cbacd6182056f9efd1d070ce
lichengcheng <lichengcheng@pset.suntec.net>
2017-09-12 17:41:45 +0800

    add member function for class
    Change-Id: Ifac16975aca5eb2296792e88c853701625abe9bc

--- c++/box_class.cpp
+++ c++/box_class.cpp
@@ -3,6 +3,7 @@
 	> Created Time: Tue 12 Sep 2017 05:04:53 PM CST
 	类的对象的公共数据成员可以使用直接成员访问运算符 (.) 来访问
 	私有的成员和受保护的成员不能使用直接成员访问运算符 (.) 来直接访问
+	类的成员函数是指那些把定义和原型写在类定义内部的函数，就像类定义中的其他变量一样。类成员函数是类的一个成员，它可以操作类的任意对象，可以访问对象中的所有成员。
  ************************************************************************/
 
 #include<iostream>
@@ -14,7 +15,36 @@ class Box
 		double length;
 		double breadth;
 		double height;
+
+		double getVolume(void);
+/*		{
+			return length*height*breadth;
+		}
+*/
+		void setLength(double len);
+		void setBreadth(double bre);
+		void setHeight(double hei);
 };
+
+double Box::getVolume(void)
+{
+	return length*height*breadth;
+}
+
+void Box::setLength(double len)
+{
+	length = len;
+}
+
+void Box::setBreadth(double bre)
+{
+	breadth = bre;
+}
+
+void Box::setHeight(double hei)
+{
+	height = hei;
+}
 
 int main()
 {
@@ -23,20 +53,20 @@ int main()
 	double volume = 0.0;
 
 	/* this is Box1 config */
-	Box1.height = 5.0;
-	Box1.length = 6.0;
-	Box1.breadth = 7.0;
+	Box1.setHeight(5.0);
+	Box1.setLength(6.0);
+	Box1.setBreadth(7.0);
 
 	/* this is Box2 config */
-	Box2.height = 10.0;
-	Box2.length = 12.0;
-	Box2.breadth = 13.0;
+	Box2.setHeight(10.0);
+	Box2.setLength(12.0);
+	Box2.setBreadth(13.0);
 
-	volume = Box1.height*Box1.length*Box1.breadth;
+	volume = Box1.getVolume();
 	cout << "Box1' volume:"<< volume <<endl;
 
 
-	volume = Box2.height*Box2.length*Box2.breadth;
+	volume = Box2.getVolume();
 	cout << "Box2' volume:"<< volume <<endl;
 
 	return 0;
