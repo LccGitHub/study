ace554bf8e14616e8354a390e8d3863214e80714
lichengcheng <lichengcheng@pset.suntec.net>
2018-01-16 15:09:41 +0800

    packeage function
    Change-Id: I8ca5253253887de30c7c236be702e2a3cc3a0606

--- python/reptile/file.py
+++ python/reptile/file.py
@@ -1,44 +1,107 @@
 #!/usr/bin/env python
 # -*- coding:utf-8 -*-
 
+import sys
+import getopt, string
+from os import path,makedirs
+import os.path
 import re
+
+str = ""
+function = ""
 
 classname = ""
 functionname = []
 functiontype = []
 functionparam = []
-file = open("../../c++/dbclass.h") 
-for line in file:
-    l = line.strip()
-    if l.startswith("class"):
-        pattern = re.compile("^class (.*)")
-        temp = re.match(pattern, l)
-        if temp:
-            classname =  temp.group(1)
-            print classname
-    else:
-        pattern = re.compile("(.*) (.*)\((.*)\);$")
-        temp = re.match(pattern, l)
-        if temp:
-            functionname.append(temp.group(2))
-            functiontype.append(temp.group(1))
-            functionparam.append(temp.group(3))
-        print functiontype
-        print functionname
-        print functionparam
-file.close()
+
+h_file_name = ""
+c_file_name = ""
+def readFile():
+    file = open(h_file_name, "rb")
+    for line in file:
+        l = line.strip()
+        if l.startswith("class"):
+            pattern = re.compile("^class (.*)[:].*[^;]$")
+            temp = re.match(pattern, l)
+            if temp:
+                global classname
+                classname =  temp.group(1)
+                print classname
+        else:
+             pattern = re.compile("(.*) (.*)\((.*)\);$")
+             temp = re.match(pattern, l)
+             if temp:
+                 functionname.append(temp.group(2))
+                 functiontype.append(temp.group(1))
+                 functionparam.append(temp.group(3))
+                 #print functiontype
+                 #print functionname
+                 #print functionparam
+    file.close()
 
 
-str = ""
-function = ""
-lenght = len(functiontype)
-print lenght
+def generateCode():
+    global str
+    str = '#include"dbclass.h"\n\n\n\n'
+    lenght = len(functiontype)
+    print lenght
+    for i in range(lenght):
+        function = functiontype[i] + " " +classname + "::"+ functionname[i]+"( "+functionparam[i]+ " )\n{\n}\n\n"
+        str = str + function
 
-str = '#include"dbclass.h"\n\n\n\n'
-for i in range(lenght):
-    function = functiontype[i] + " " +classname + "::"+ functionname[i]+"( "+functionparam[i]+ " )\n{\n}\n\n" 
-    str = str + function
 
-writef = open("../../c++/dbclass.cpp", "wb+")
-writef.write(str)
-writef.close()
+def writeFile():
+    getCfilepath()
+    print c_file_name
+    writef = open(getCfilepath(), "wb+")
+    print writef.mode
+    writef.write(str)
+    writef.close()
+
+def getCfilepath():
+    c_file_name = re.sub(r'.h', '.cpp', h_file_name)
+    print c_file_name
+    return c_file_name
+def usage():
+        """
+        Help for the command line parser.
+        """
+        print 'cpp file Generator.'
+        print 'Usage:'
+        print ' -h: help'
+        print ' -i: file'
+        print ' --help: help'
+        print ' --in: help'
+        sys.exit(1)
+
+def Entry():
+    EntryParams()
+    getCfilepath()
+    readFile()
+    generateCode()
+    writeFile()
+
+def EntryParams():
+    try:
+            opts, args = getopt.getopt(sys.argv[1:],'hi:', ['in=', 'help'])
+            print opts
+    except:
+            print '------------exception------------'
+            usage()
+
+    for (opt,arg) in opts:
+        global h_file_name
+
+        if opt in ['-h', '--help']:
+            usage()
+        elif opt in ['-i', '--in']:
+            if arg != "":
+                h_file_name = arg
+            else:
+                h_file_name = None
+
+if __name__ == '__main__':
+    Entry()
+    #print h_file_name
+
